660d0161cb6ec6455d1525d029ff09362dcf7faf
Danny McDonald <mcddjx@gmail.com>
2020-05-05 18:30:58 +0200

    setup all at once

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
+from setuptools import setup, Extension, dist
 import setuptools
 import sys
 
+dist.Distribution().fetch_build_eggs(["Cython>=0.15.1", "numpy>=1.10"])
+
 try:
     from Cython.Build import cythonize
+
     USE_CYTHON = True
 except ImportError:
-    sys.exit("""Could not import Cython, which is required to build benepar extension modules.
-Please install cython and numpy prior to installing benepar.""")
+    sys.exit(
+        """Could not import Cython, which is required to build benepar extension modules.
+Please install cython and numpy prior to installing benepar."""
+    )
 
 try:
     import numpy as np
 except ImportError:
-    sys.exit("""Could not import numpy, which is required to build the extension modules.
-Please install cython and numpy prior to installing benepar.""")
+    sys.exit(
+        """Could not import numpy, which is required to build the extension modules.
+Please install cython and numpy prior to installing benepar."""
+    )
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -31,18 +39,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nikitakit/self-attentive-parser",
     packages=setuptools.find_packages(),
-    package_data={'': ['*.pyx']},
-    ext_modules = cythonize(extensions),
+    package_data={"": ["*.pyx"]},
+    ext_modules=cythonize(extensions),
     classifiers=(
-        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Text Processing :: Linguistic",
     ),
-    setup_requires = ["cython", "numpy"],
-    install_requires = ["cython", "numpy", "nltk>=3.2"],
+    setup_requires=["cython", "numpy"],
+    install_requires=["cython", "numpy", "nltk>=3.2"],
     extras_require={
         "cpu": ["tensorflow>=1.11.0"],
         "gpu": ["tensorflow-gpu>=1.11.0"],
